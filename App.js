d166cc29a7a874af354711ec6afca54029182fa6
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 18:24:14 +0200

    Add a Box component
    This commit adds a Box component to the App.js file.
    
    The Box component is a mesh with a boxGeometry and a meshStandardMaterial.
    
    The App component now uses the Box component instead of the mesh component.

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { Canvas } from '@react-three/fiber';
 
+function Box(props) {
+  return (
+    <mesh {...props}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  )
+}
+
 export default function App() {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-
-      <mesh position={[-1, -1, 0]} scale={0.1}>
-        <torusKnotGeometry radius={10} args={[10, 1, 260, 6, 10, 16]} />
-        <meshStandardMaterial color="green" />
-      </mesh>
+      <Box />
+      <Box position={[0, 2, 0]} />
+      <Box position={[0, -2, 0]} />
     </Canvas>
   );
 }

5c70b7af6f527234d636546aa404799277a6165a
Rokas <rookas.rudzenskas@gmail.com>
2023-02-20 19:03:47 +0200

    Add textures to shoe
    The textures are loaded using the TextureLoader from expo-three.
    
    The textures are then applied to the shoe using the useLayoutEffect hook.

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import {Suspense, useRef, useState} from "react";
+import {Suspense, useLayoutEffect, useRef, useState} from "react";
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import {ActivityIndicator, View} from "react-native";
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { TextureLoader } from 'expo-three';
 
 function Box(props) {
   const [active, setActive] = useState(false);
@@ -29,8 +30,28 @@ function Box(props) {
 }
 
 function Shoe(props) {
+  const [base, normal, rough] = useLoader(TextureLoader, [
+    require('./assets/Airmax/textures/BaseColor.jpg'),
+    require('./assets/Airmax/textures/Normal.jpg'),
+    require('./assets/Airmax/textures/Roughness.png'),
+  ]);
+
   const material = useLoader(MTLLoader, require('./assets/Airmax/shoe.mtl'));
-  const obj = useLoader(OBJLoader, require('./assets/Airmax/shoe.obj'));
+  const obj = useLoader(OBJLoader, require('./assets/Airmax/shoe.obj'), (loader) => {
+    material.preload();
+    loader.setMaterials(material);
+  });
+
+
+  useLayoutEffect(() => {
+    obj.traverse((child) => {
+      if(child instanceof THREE.Mesh) {
+        child.material.map = base;
+        child.material.normalMap = normal;
+        child.material.roughnessMap = rough;
+      }
+    })
+  }, [obj]);
 
 
   return (
